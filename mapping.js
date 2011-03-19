1b79edae7cabfa06a9b4d95d2ab0947f7ed555e2
devdave <devdave@ominian.net>
2011-03-18 21:27:21 -0600

    Figured out issues with rendering quadrant borders, now working on performance

--- mapping.js
+++ mapping.js
@@ -1,5 +1,7 @@
 
 /**
+ *Quadrant is a recursive quad-tree like structure that
+ *breaks a given area progressivel into 4 quadrants.
  *
  *
  *@argument {Integer} mx Upper left x coordinate
@@ -16,107 +18,136 @@ function Quadrant(x, y, sx, sy, depth, name){
     this.depth = depth - 1;
     this.entities = [];
     this.name = name || "root";
-    /* @property {Quadrant} left */
+    /* @property {Quadrant} ul Upper left */
     this.ul = null;
-    /* @property {Quadrant} left */
+    /* @property {Quadrant} ll Lower left */
     this.ll = null;
-    /* @property {Quadrant} right */
+    /* @property {Quadrant} ur Upper right*/
     this.ur = null;
-    /* @property {Quadrant} right */
+    /* @property {Quadrant} lr Lower right*/
     this.lr = null;
-    if(this.depth > 0){
-        var halfX = this.sx / 2;
-        var halfY = this.sy / 2;
-        this.ul = new Quadrant(this.x, this.y, halfX , halfY , depth - 1, this.name + " -> ul");
-        
-        this.ur = new Quadrant(this.x + halfX, this.y,  halfX , halfY , depth - 1, this.name + " -> ur");
-        
-        this.ll = new Quadrant(this.x, this.y + halfY , halfX , halfY , depth - 1, this.name + " -> ll" );
+    
 
-        this.lr = new Quadrant( this.x + halfX, this.y + halfY, halfX ,  halfY  , depth - 1, this.name + " ->lr");
-    }
-    console.groupEnd();
 }
+
 
 /**
  *Does this quadrant contain this point?
  *
+ *@TODO extract this to possible Shapes library
  *@argument x is either an X coordinate or a point
  *@argument {Integer} y
  */
 Quadrant.prototype.contains  = function(x, y){
-    var point = (arguments.length == 1) ? x : [x,y];
-    if( point[0] >= this.x && point[0] <= this.x + this.sx){
-        if(point[1] >= this.y && point[1] <= this.y + this.sy){
-            return true;
-        }
-    }
-    
-    return false;
-    
+    return (x > this.x  && x < this.x + this.sx)
+        && (y > this.y  && y < this.y + this.sy);
 }
 
+/**
+ *High level Entity Add logic, focuses on where to route
+ *a new entity.
+ *
+ *@property {x,y,sx,sy} All information needed to map out a box shape
+ */
 Quadrant.prototype.add = function(entity){
-    this.entities.unshift(entity);
+    
+    //Does this quadrant divided already?
+    if(this.lr || this.ll || this.ul || this.ur){
+        this.loop(function(){
+            if(entity.x > this.x && entity.x < this.x + this.sx){
+                if(entity.y > this.y && entity.y < this.y + this.sy){
+                    this.add(entity);
+                }
+            }
+        });
+    }
+    //Should this quadrant be divided?
+    else if(this.entities.length > 1 && this.depth > 0){
+        //This Quadrant has become crowded, flush out all entities down the next level
+        this.spawn();
+        this.entities.push(entity);
+        this.divide();
+        
+    }else{
+        //No, push entity to this Quadrant
+        this.entities.push(entity);
+    }
 }
+
+/**
+ *Spawns child nodes
+ *
+ */
+Quadrant.prototype.spawn = function(){
+    
+    var sizeX = this.sx / 2;
+    var sizeY = this.sy / 2;
+    
+    this.ul = new Quadrant(this.x,          this.y,         sizeX, sizeY,   this.depth - 1, "ul");     
+    this.ur = new Quadrant(this.x + sizeX,  this.y,         sizeX, sizeY,   this.depth - 1, "ur");
+    this.ll = new Quadrant(this.x,          this.y + sizeY, sizeX, sizeY,   this.depth - 1, "ll");
+    this.lr = new Quadrant(this.x + sizeX,  this.y + sizeY, sizeX, sizeY,   this.depth - 1, "lr");
+}
+
+/**
+ *Divide current Quadrant
+ *
+ */
+Quadrant.prototype.divide = function(){
+    var entity = null;
+    while(entity = this.entities.pop()){
+        this.loop(function(){
+            if(entity.x > this.x && entity.x < this.x + this.sx){
+                if(entity.y > this.y && entity.y < this.y + this.sy){
+                    this.add(entity);
+                }
+            }
+        })
+    }
+}
+
 
 Quadrant.prototype.render = function(ctx, depth){
-    ctx.strokeRect(this.x, this.y, this.x + this.sx, this.y + this.sy);
+    ctx.strokeRect(this.x, this.y, this.sx, this.sy);
     this.loop(function(){
         this.render(ctx, depth - 1);
     });
 }
 
 Quadrant.prototype.loop = function(block){
+    var retvals = [];
     var points = ["ul","ll",'ur','lr'];
     for(var i = 0; i < points.length; i++ ){
         if( this[points[i]] instanceof Quadrant ){
-            try{
-                block.call(this[points[i]], points[i]);                               
-            }catch(e){
-                if(console && console.log){
-                    console.log(e);
-                }
-            }
+            retvals.push( block.call(this[points[i]], points[i]));
         }
     }
+    return retvals;
 }
 
-Quadrant.prototype.find = function(point){
+Quadrant.prototype.find = function(x,y){
     var targets = [];
-    if(this.contains(point)){
+    if(this.contains(x,y)){
         targets.push(this)
     }else{
         return [];
     }
-    targets.concat(this.loop(function(name){
-        targets = targets.concat(this.find(point));
-    }));
+    this.loop(function(){
+        targets = targets.concat(this.find(x,y));
+    });
     return targets;
 }
 
-function EntityMap(ctx, max){
-    this.maxX = ctx.canvas.clientWidth;
-    this.maxY = ctx.canvas.clientHeight;
-    this.maxZones = max || 4;
-    this.root = new Quadrant(0,0,this.maxX , this.maxY , this.maxZones);
-    
+function QuadrantFactory(ctx, max){
+    return new Quadrant(0,0, ctx.canvas.clientWidth, ctx.canvas.clientHeight, max || 4 )
 }
 
-EntityMap.prototype.find = function(point){
-    if(this.root.contains(point[0],point[1]) === false){
-        return [];
-    }
-    return this.root.find(point);
-    
-}
 
-/**
- *
- *@argument {CanvasRenderingContext2D} ctx
- */
-EntityMap.prototype.render = function(ctx, depth){
-    this.root.render(ctx, depth);
-}
+
+
+(function(){
+    console.log("Test here!");
+}());
+
 
 
